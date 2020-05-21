cf9c13e4e1357f238361962a1a18e8468b537f7a
elsa chen <snowing222@gmail.com>
2020-05-21 11:09:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday_supplies_hash|
     puts "#{season.to_s.capitalize}:"
    holiday_supplies_hash.each do |holiday, supplies_array|
-      
+
       puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies_array.join(", ")}"
 
     end
